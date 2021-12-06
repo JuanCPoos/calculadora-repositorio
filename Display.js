2093b1183a542d983ea05d3e7795d9957ff1324c
JuanCPoos <juan.cpoos@gmail.com>
2021-12-06 18:25:41 -0300

    Cambios finales

--- Display.js
+++ Display.js
@@ -1,6 +1,6 @@
 class Display{
 
-    constructor(displayValorAnterior, displayValorActual) {
+     constructor(displayValorAnterior, displayValorActual) {
         this.displayValorActual = displayValorActual;
         this.displayValorAnterior = displayValorAnterior;
         this.calculador = new Calculadora();
@@ -11,48 +11,46 @@ class Display{
             sumar: '+',
             dividir: '%',
             multiplicar: 'x',
-            restar: '-',
+            restar: '-', 
         }
-
-    }        
-    computar(tipo){
-        this.tipoOperacion !== 'igual' && this.calculate();
-        this.tipoOperacion = tipo
-        this.valorAnterior = this.valorActual || this.valorAnterior
-        this.valorActual = " ";
-        this.printVal()
     }
 
-    delete(){
-        this.valorActual = this.valorActual.toString().slice(0,-1)
-        this.printVal()
+    borrar() {
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
     }
 
-    deleteAll(){
-        this.valorActual = " "
-        this.valorAnterior = " " 
-        this.tipoOperacion = undefined
-        this.printVal()
+    borrarTodo() {
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
     }
 
-    calculate(){
-        const valorAnterior = parseFloat(this.valorAnterior)
-        const valorActual = parseFloat(this.valorActual)
-
-        if(isNan(valorActual) || isNan(valorAnterior)) return
-        this.valorActual = this.calculadora[this.tipoOperacion](valorAnterior, valorActual)
-
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
     }
 
-    agregarNumero(parametro) {
-        if(parametro === '.' && this.valorActual.includes('.')) return 
-        this.valorActual = this.valorActual.toString() + parametro.toString();
-        this.printVal()
+    agregarNumero(numero) {
+        if(numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
     }
 
-    printVal() {
-        this.displayValorActual.textContent = this.valorActual 
-        //this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || 'pasa'}`
+    imprimirValores() {
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
     }
 
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if( isNaN(valorActual)  || isNaN(valorAnterior) ) return
+        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
+    }
 }
--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const display = new Display(displayValorAnterior, displayValorActual)
 
 botonesNumeros.forEach(boton => {
     boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML))
-})
+});
 
 botonesOperadores.forEach(boton => {
     boton.addEventListener('click', () => display.computar(boton.value))
-})  
+}); 
